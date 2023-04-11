4dfdef24cd861b20542fc0ebb7dfb7747bfd9ad5
lauracamilaballesteros <92760450+lauracamilaballesteros@users.noreply.github.com>
2023-04-10 22:47:08 -0500

    fin

--- pregunta.py
+++ pregunta.py
@@ -12,7 +12,20 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
+    df = df.dropna()
+    df = df.drop_duplicates()
+    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x.replace('$', '')).apply(lambda x: x.replace(',', '')).astype(float).astype(int)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'])
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
+    df[['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']] =df[['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']].apply(lambda x: x.str.lower())
+    df[['idea_negocio', 'línea_credito']] = df[['idea_negocio', 'línea_credito']].str.replace('_',' ').str.replace('-',' ').str.strip()
+    
+    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
+    
+    df['idea_negocio'].apply(lambda x: x.replace(' ', '_'))
+    
+    df.drop(['Unnamed: 0'], axis=1)
+    
     #
     # Inserte su código aquí
     #
